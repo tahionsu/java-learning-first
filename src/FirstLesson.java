6c48e6f66e8139f6f64bb923fa304314228ec96e
Anton Milov <avm@gldva.ru>
2022-10-21 13:27:15 +0700

    Add second task

--- src/FirstLesson.java
+++ src/FirstLesson.java
@@ -1,50 +1,37 @@
 public class FirstLesson {
     public static void main(String[] args) {
         System.out.println(getMonthName(1));
+
+        char[] arrayChar = {'a', 'b', 'c', 'd', '1'};
+        System.out.println(searchValInArray(arrayChar, 'e'));
     }
 
     public static String getMonthName(int monthNum){
-        String monthName;
-        switch(monthNum){
-            case 1:
-                monthName = "January";
-                break;
-            case 2:
-                monthName = "February";
-                break;
-            case 3:
-                monthName = "March";
-                break;
-            case 4:
-                monthName = "April";
-                break;
-            case 5:
-                monthName = "May";
-                break;
-            case 6:
-                monthName = "June";
-                break;
-            case 7:
-                monthName = "July";
-                break;
-            case 8:
-                monthName = "August";
-                break;
-            case 9:
-                monthName = "September";
-                break;
-            case 10:
-                monthName = "October";
-                break;
-            case 11:
-                monthName = "November";
-                break;
-            case 12:
-                monthName = "December";
-                break;
-            default:
-                monthName = "Not valid month number";
+        return switch (monthNum) {
+            case 1 -> "January";
+            case 2 -> "February";
+            case 3 -> "March";
+            case 4 -> "April";
+            case 5 -> "May";
+            case 6 -> "June";
+            case 7 -> "July";
+            case 8 -> "August";
+            case 9 -> "September";
+            case 10 -> "October";
+            case 11 -> "November";
+            case 12 -> "December";
+            default -> "Not valid month number";
+        };
+    }
+
+    public static int searchValInArray(char[] array, char ch){
+        int index = -1;
+
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == ch){
+                index = i;
+            }
         }
-        return monthName;
+        return index;
     }
 }
